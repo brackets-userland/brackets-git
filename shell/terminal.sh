8e003eff4e56a7e0035dccf1dcda1950108e6031
Martin Zagora <zaggino@gmail.com>
2014-02-17 16:02:20 -0500

    Modify terminal script for linux as suggested by @benjaminpick

--- shell/terminal.sh
+++ shell/terminal.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-if hash gnome-terminal 2>/dev/null
+if command -v gnome-terminal >/dev/null 2>&1
 then
     gnome-terminal --window --working-directory="$1"
+    echo ok
+elif command -v konsole >/dev/null 2>&1
+then
+    konsole --workdir "$1"
+    echo ok
+elif command -v xterm >/dev/null 2>&1
+then
+    xterm -e 'cd $1 && bash'
     echo ok
 fi
