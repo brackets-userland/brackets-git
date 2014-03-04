d99859dfb203654960ed7f1f25049daf970efac6
Ben Keith <ben@benlk.com>
2014-03-03 23:58:23 -0500

    Adds xfce4-terminal as an option
    Because I like to use it, and XFCE is included in one of the main Ubuntu distributions, Xubuntu.
    
    I foresee future additions of the following terminals:
    
    - aterm
    - guake
    - lxterminal
    - rxvt
    - terminator
    - tilda
    - urxvt
    - yakuake

--- shell/terminal.sh
+++ shell/terminal.sh
@@ -7,6 +7,10 @@ elif command -v konsole >/dev/null 2>&1
 then
     konsole --workdir "$1"
     echo ok
+elif command -v xfce4-terminal >/dev/null 2>&1
+then
+    xfce4-terminal --window --working-directory="$1"
+    echo ok
 elif command -v xterm >/dev/null 2>&1
 then
     xterm -e 'cd $1 && bash'
