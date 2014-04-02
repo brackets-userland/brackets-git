192a6a36b029717076e0b1b3efbe33083e4a8189
Martin Zagora <zaggino@gmail.com>
2014-04-02 15:27:45 +1100

    rewrite main.js

--- main.js
+++ main.js
@@ -5,41 +5,32 @@
  * @license http://opensource.org/licenses/MIT
  */
 
-/*jslint plusplus: true, vars: true, nomen: true */
-/*global define, brackets */
-
 define(function (require, exports, module) {
-    "use strict";
 
-    // Get module dependencies.
-    var AppInit                    = brackets.getModule("utils/AppInit"),
-        CommandManager             = brackets.getModule("command/CommandManager"),
-        Commands                   = brackets.getModule("command/Commands"),
-        ExtensionUtils             = brackets.getModule("utils/ExtensionUtils"),
-        Menus                      = brackets.getModule("command/Menus");
+    // Brackets modules
+    var AppInit         = brackets.getModule("utils/AppInit"),
+        CommandManager  = brackets.getModule("command/CommandManager"),
+        Commands        = brackets.getModule("command/Commands"),
+        ExtensionUtils  = brackets.getModule("utils/ExtensionUtils"),
+        Menus           = brackets.getModule("command/Menus");
 
-    var ExtensionInfo              = require("src/ExtensionInfo"),
-        Preferences                = require("src/Preferences"),
-        ExtensionMain              = require("src/Main"),
-        ChangelogDialog            = require("src/ChangelogDialog"),
-        SettingsDialog             = require("src/SettingsDialog"),
-        Strings                    = require("strings");
+    // Local modules
+    var ChangelogDialog = require("src/ChangelogDialog"),
+        ExtensionInfo   = require("src/ExtensionInfo"),
+        Main            = require("src/Main"),
+        Preferences     = require("src/Preferences"),
+        SettingsDialog  = require("src/SettingsDialog"),
+        Strings         = require("strings");
 
     // Load extension modules that are not included by core
     var modules = ["src/Remotes"];
-    if (Preferences.get("useGitFtp")) {
-        modules.push("src/Ftp/Ftp");
-    }
+    if (Preferences.get("useGitFtp")) { modules.push("src/Ftp/Ftp"); }
     require(modules);
 
     // Load CSS
     ExtensionUtils.loadStyleSheet(module, "styles/brackets-git.less");
     ExtensionUtils.loadStyleSheet(module, "styles/fonts/octicon.less");
-
-    // Handle settings dialog
-    function openSettingsPanel() {
-        SettingsDialog.show();
-    }
+    // TODO: load ftp.less on demand after Sprint 38
 
     // Display settings panel on first start / changelog dialog on version change
     ExtensionInfo.get().then(function (packageJson) {
@@ -48,7 +39,7 @@ define(function (require, exports, module) {
 
         if (lastVersion === null) {
             Preferences.persist("lastVersion", "firstStart");
-            openSettingsPanel();
+            SettingsDialog.show();
         } else if (lastVersion !== currentVersion) {
             Preferences.persist("lastVersion", currentVersion);
             ChangelogDialog.show();
@@ -57,11 +48,10 @@ define(function (require, exports, module) {
 
     // Register command and add it to the menu.
     var SETTINGS_COMMAND_ID = "brackets-git.settings";
-    CommandManager.register(Strings.GIT_SETTINGS, SETTINGS_COMMAND_ID, openSettingsPanel);
+    CommandManager.register(Strings.GIT_SETTINGS, SETTINGS_COMMAND_ID, SettingsDialog.show);
     Menus.getMenu(Menus.AppMenuBar.FILE_MENU).addMenuItem(SETTINGS_COMMAND_ID, "", Menus.AFTER, Commands.FILE_PROJECT_SETTINGS);
 
     AppInit.appReady(function () {
-        ExtensionMain.init();
+        Main.init();
     });
-
 });
