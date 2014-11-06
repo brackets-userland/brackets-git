a3cb27cf01d0b6eff4dfa86c2e2eed969dfab234
Martin Zagora <zaggino@gmail.com>
2014-11-06 15:45:25 +1100

    --window argument causes xfce4-terminal to open twice

--- shell/terminal.sh
+++ shell/terminal.sh
@@ -7,7 +7,7 @@ then
     konsole --workdir "$1"
 elif command -v xfce4-terminal >/dev/null 2>&1
 then
-    xfce4-terminal --window --working-directory="$1"
+    xfce4-terminal --working-directory="$1"
 elif command -v xterm >/dev/null 2>&1
 then
     xterm -e 'cd $1 && bash'
