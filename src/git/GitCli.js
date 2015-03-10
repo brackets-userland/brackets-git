f2f35332bf4b8bd65f0e274f971a5b70482ef5c7
Ignacio Moreno <ignacio@genuitec.com>
2015-03-10 09:41:11 -0600

    - Use array concat instead for using forEach
    - return the method on the promises
    - catch error on promises
    - remove not required commitMode check

--- src/git/GitCli.js
+++ src/git/GitCli.js
@@ -547,9 +547,7 @@ define(function (require, exports) {
         if (updateIndex) { args.push("-u"); }
         if ($.isArray(file)) {
             args.push("--");
-            _.forEach(file, function (next) {
-                args.push(next);
-            });
+            args.concat(file);
         } else {
             args.push("--", file);
         }
@@ -784,9 +782,7 @@ define(function (require, exports) {
         var args = ["diff", "--no-ext-diff", "--no-color", "--full-index"];
         if (files !== undefined) {
             if ($.isArray(files)) {
-                _.forEach(function (next) {
-                    args.push(next);
-                });
+                args.concat(files);
             } else {
                 args.push(files);
             }
