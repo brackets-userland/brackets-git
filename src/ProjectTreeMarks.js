d75c8159381e90020c5dc6fb7d49a244bc7e0f27
Phillip Haydon <phillip.haydon@gmail.com>
2015-05-17 19:22:47 +0800

    fixes issue where folder depth is always 1 or more instead of 0 or more

--- src/ProjectTreeMarks.js
+++ src/ProjectTreeMarks.js
@@ -92,7 +92,7 @@ define(function (require) {
                 // are external files with the same name as a project file
                 regex = regexEscape(gitRoot) + (leadingSlash ? "" : "((.+)/)?") + regexEscape(line) + (trailingSlash ? "" : "(/.{0,})?");
                 // replace all the possible asterisks
-                regex = regex.replace(/\*\*$/g, "(.{0,})").replace(/(\*\*|\*$)/g, "(.+)").replace(/\*/g, "([^/]+)");
+                regex = regex.replace(/\*\*$/g, "(.{0,})").replace(/(\*\*|\*$)/g, "(.+)").replace(/\*/g, "([^/]*)");
                 regex = "^" + regex + "$";
 
                 return {
