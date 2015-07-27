4f083f25b5c256b6f34e997f937d7319d76698c8
Martin Zagora <zaggino@gmail.com>
2015-07-27 10:50:30 +1000

    rewritten event-emitter

--- src/brackets.js
+++ src/brackets.js
@@ -1,3 +1,4 @@
+export const _ = brackets.getModule('thirdparty/lodash');
 export const CommandManager = brackets.getModule('command/CommandManager');
 export const Commands = brackets.getModule('command/Commands');
 export const ExtensionUtils = brackets.getModule('utils/ExtensionUtils');
--- src/event-emitter.js
+++ src/event-emitter.js
@@ -1,1 +1,45 @@
-export const eventEmitter = true;
+import EventEmitter from 'eventemitter';
+import Preferences from './preferences';
+import { log } from './log';
+
+const debugOn = Preferences.get('debugMode');
+const emInstance = new EventEmitter({ wildcard: false });
+
+if (debugOn) {
+  emInstance._emit = emInstance.emit;
+  emInstance.emit = function (eventName, ...args) {
+
+    if (!eventName) {
+      throw new Error('no event has been thrown!');
+    }
+
+    let listenersCount = this.listeners(eventName).length;
+    let argsString = args.map(function (arg) {
+      if (arg === null) {
+        return 'null';
+      }
+      if (typeof arg === 'undefined') {
+        return 'undefined';
+      }
+      if (typeof arg === 'function') {
+        return 'function(){...}';
+      }
+      if (!arg.toString) {
+        return Object.prototype.toString.call(arg);
+      }
+      return arg.toString();
+    }).join(', ');
+
+    if (argsString) {
+      argsString = ' - ' + argsString;
+    }
+
+    argsString = argsString + ' (' + listenersCount + ' listeners)';
+
+    log('Event invoked: ' + eventName + argsString);
+
+    return this._emit.apply(this, arguments);
+  };
+}
+
+export default emInstance;
