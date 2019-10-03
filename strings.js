6b9dcb3b4cebe86817e7c798a647336b7156cdf4
archit10uniyal <archituniyal5@gmail.com>
2019-10-04 00:20:06 +0530

    Changes made to the comments in strings.js for better understanding of the code.

--- strings.js
+++ strings.js
@@ -1,8 +1,8 @@
 /*
- * This file provides the interface to user visible strings in Brackets. Code that needs
- * to display strings should should load this module by calling var Strings = require("strings").
- * The i18n plugin will dynamically load the strings for the right locale and populate
- * the exports variable. See nls/root/strings.js for the master file of English strings.
+ * This file provides the interface to user visible strings in Brackets. 
+ * Code that needs to display strings, should load this module by calling var Strings = require("strings").
+ * The i18n plugin will dynamically load the strings for the right locale and populate the exports variable.
+ * See nls/root/strings.js for the master file of English strings.
  */
 define(function (require, exports, module) {
     module.exports = require("i18n!nls/strings");
